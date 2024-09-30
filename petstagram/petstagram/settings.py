7ddcb7b1b431e9558a1c3f91b7b13117404b4e59
Boris Popov <32306686+bpopovgit@users.noreply.github.com>
2024-09-30 21:03:41 +0300

    feat: connect to db, add accounts, pets, photos, common apps and templates

--- petstagram/petstagram/settings.py
+++ petstagram/petstagram/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'petstagram.photos',
+    'petstagram.pets',
+    'petstagram.accounts',
+    'petstagram.common',
 ]
 
 MIDDLEWARE = [
@@ -75,9 +80,13 @@ WSGI_APPLICATION = 'petstagram.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "petstagram_db",
+        "USER": "postgres-user",
+        "PASSWORD": "password",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
 
