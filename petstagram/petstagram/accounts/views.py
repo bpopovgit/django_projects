01575c0dde20f9778833a638703134f75e3f4169
Boris Popov <32306686+bpopovgit@users.noreply.github.com>
2024-09-30 21:51:48 +0300

    feat: added urls and views to accounts, common, pets and photos

--- petstagram/petstagram/accounts/views.py
+++ petstagram/petstagram/accounts/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def register(request):
+    return render(request, template_name='accounts/register-page.html')
+
+
+def login(request):
+    return None
+
+
+def show_profile_details(request):
+    return None
+
+
+def edit_profile(request):
+    return None
+
+
+def delete_profile(request):
+    return None
