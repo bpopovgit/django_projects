0daa20720d594dd0ab13b47dda009a5e1ac21bb4
Boris Popov <32306686+bpopovgit@users.noreply.github.com>
2024-10-06 17:59:11 +0300

    feat: added new template tags, the edit-post view and adjusted the posts under templates

--- forumApp/forumApp/posts/views.py
+++ forumApp/forumApp/posts/views.py
@@ -3,7 +3,7 @@ from datetime import datetime
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 
-from forumApp.posts.forms import PostBaseForm, PostCreateForm, PostDeleteForm, SearchForm
+from forumApp.posts.forms import PostBaseForm, PostCreateForm, PostDeleteForm, SearchForm, PostEditForm
 from forumApp.posts.models import Post
 
 
@@ -48,7 +48,23 @@ def add_post(request):
 
 
 def edit_post(request, pk: int):
-    return HttpResponse()  # TODO: fix it
+    post = Post.objects.get(pk=pk)
+
+    if request.method == 'POST':
+        form = PostEditForm(request.POST, instance=post)
+
+        if form.is_valid():
+            form.save()
+            return redirect('dash')
+    else:
+        form = PostEditForm(instance=post)
+
+    context = {
+        "form": form,
+        "post": post,
+    }
+
+    return render(request, 'posts/edit-post.html', context)
 
 
 def details_page(request, pk: int):
@@ -74,4 +90,4 @@ def delete_post(request, pk: int):
         "post": post,
     }
 
-    return render(request, 'posts/delete-template.html', context)
+    return render(request, 'posts/delete-post.html', context)
